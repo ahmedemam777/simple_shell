6db4eefc31e258e21669311521b3de3971a2dc0d
ahmedemam777 <ahmedemam201028@gmail.com>
2023-11-05 00:19:33 +0200

    second step

---
+++ header.h
@@ -0,0 +1,17 @@
+#ifndef HEADER
+#define HEADER
+/*libraries*/
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+#include <signal.h>
+
+/*STRING FUNCS*/
+
